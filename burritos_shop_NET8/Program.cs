96dcf96ca90c030607322ba5ab0369cf36cd8d55
Afnan Rafid <arafid@ou.edu>
2026-02-10 12:48:58 -0600

    main

--- burritos_shop_NET8/Program.cs
+++ burritos_shop_NET8/Program.cs
@@ -85,9 +85,37 @@ class Program
 
         // Please Enter Your For Loop Codes Here
 
+        decimal unitprice = 9.00m;
+        decimal mincost = 1200.00m;
+        decimal unitcost = 3.80m;
+
+        int itemsold = 0;
+
+        for (itemsold = 0; ; itemsold++)
+        {
+            decimal revenue = unitprice * itemsold;
+            decimal cost = mincost + (unitcost * itemsold);
+            decimal profit = revenue - cost;
+
+            if (profit <= 0)
+            {
+                Console.WriteLine($"After selling {itemsold} burritos: profit {profit:F2}.");
+            }
+            
+            else
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("Success! Profit reached.");
+                Console.WriteLine($"Number of Burritos: {itemsold}");
+                Console.WriteLine($"Revenue: ${revenue:C2}");
+                Console.WriteLine($"Cost: ${cost:C2}");
+                Console.WriteLine($"Profit: ${profit:C2}");
+                break;
+            }
+        }
 
 
-        Console.WriteLine("\nPress any key to return to menu...");
+            Console.WriteLine("\nPress any key to return to menu...");
         Console.ReadKey();
     }
 
